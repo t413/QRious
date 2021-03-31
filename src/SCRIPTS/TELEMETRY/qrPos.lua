30c1535e69650006214a7b5215725439e3ec802d
Tim O'Brien <timo@t413.com>
2021-03-30 17:42:33 -0700

    Lookup tables are now _strings_ (saves memory)
    - there aren’t arrays in Lua, so
    - wrote code to convert LUTables to strings

--- src/SCRIPTS/TELEMETRY/qrPos.lua
+++ src/SCRIPTS/TELEMETRY/qrPos.lua
@@ -79,103 +79,32 @@ function Qr:modnn(x)
 end
 
 -- Galois field log table
-function Qr:getGlogLookup()
-    return {
-        0xff, 0x00, 0x01, 0x19, 0x02, 0x32, 0x1a, 0xc6, 0x03, 0xdf, 0x33, 0xee, 0x1b, 0x68, 0xc7, 0x4b,
-        0x04, 0x64, 0xe0, 0x0e, 0x34, 0x8d, 0xef, 0x81, 0x1c, 0xc1, 0x69, 0xf8, 0xc8, 0x08, 0x4c, 0x71,
-        0x05, 0x8a, 0x65, 0x2f, 0xe1, 0x24, 0x0f, 0x21, 0x35, 0x93, 0x8e, 0xda, 0xf0, 0x12, 0x82, 0x45,
-        0x1d, 0xb5, 0xc2, 0x7d, 0x6a, 0x27, 0xf9, 0xb9, 0xc9, 0x9a, 0x09, 0x78, 0x4d, 0xe4, 0x72, 0xa6,
-        0x06, 0xbf, 0x8b, 0x62, 0x66, 0xdd, 0x30, 0xfd, 0xe2, 0x98, 0x25, 0xb3, 0x10, 0x91, 0x22, 0x88,
-        0x36, 0xd0, 0x94, 0xce, 0x8f, 0x96, 0xdb, 0xbd, 0xf1, 0xd2, 0x13, 0x5c, 0x83, 0x38, 0x46, 0x40,
-        0x1e, 0x42, 0xb6, 0xa3, 0xc3, 0x48, 0x7e, 0x6e, 0x6b, 0x3a, 0x28, 0x54, 0xfa, 0x85, 0xba, 0x3d,
-        0xca, 0x5e, 0x9b, 0x9f, 0x0a, 0x15, 0x79, 0x2b, 0x4e, 0xd4, 0xe5, 0xac, 0x73, 0xf3, 0xa7, 0x57,
-        0x07, 0x70, 0xc0, 0xf7, 0x8c, 0x80, 0x63, 0x0d, 0x67, 0x4a, 0xde, 0xed, 0x31, 0xc5, 0xfe, 0x18,
-        0xe3, 0xa5, 0x99, 0x77, 0x26, 0xb8, 0xb4, 0x7c, 0x11, 0x44, 0x92, 0xd9, 0x23, 0x20, 0x89, 0x2e,
-        0x37, 0x3f, 0xd1, 0x5b, 0x95, 0xbc, 0xcf, 0xcd, 0x90, 0x87, 0x97, 0xb2, 0xdc, 0xfc, 0xbe, 0x61,
-        0xf2, 0x56, 0xd3, 0xab, 0x14, 0x2a, 0x5d, 0x9e, 0x84, 0x3c, 0x39, 0x53, 0x47, 0x6d, 0x41, 0xa2,
-        0x1f, 0x2d, 0x43, 0xd8, 0xb7, 0x7b, 0xa4, 0x76, 0xc4, 0x17, 0x49, 0xec, 0x7f, 0x0c, 0x6f, 0xf6,
-        0x6c, 0xa1, 0x3b, 0x52, 0x29, 0x9d, 0x55, 0xaa, 0xfb, 0x60, 0x86, 0xb1, 0xbb, 0xcc, 0x3e, 0x5a,
-        0xcb, 0x59, 0x5f, 0xb0, 0x9c, 0xa9, 0xa0, 0x51, 0x0b, 0xf5, 0x16, 0xeb, 0x7a, 0x75, 0x2c, 0xd7,
-        0x4f, 0xae, 0xd5, 0xe9, 0xe6, 0xe7, 0xad, 0xe8, 0x74, 0xd6, 0xf4, 0xea, 0xa8, 0x50, 0x58, 0xaf
-    }
+function Qr:glogLookup(i)
+    local lookup = "\255\000\001\025\0022\026\198\003\2233\238\027h\199K\004d\224\0144\141\239\129\028\193i\248\200\008Lq\005\138e/\225$\015\0335\147\142\218\240\018\130E\029\181\194}j'\249\185\201\154\009xM\228r\166\006\191\139bf\2210\253\226\152%\179\016\145\034\1366\208\148\206\143\150\219\189\241\210\019\092\1318F@\030B\182\163\195H~nk:(T\250\133\186=\202^\155\159\010\021y+N\212\229\172s\243\167W\007p\192\247\140\128c\013gJ\222\2371\197\254\024\227\165\153w&\184\180|\017D\146\217#\032\137.7?\209[\149\188\207\205\144\135\151\178\220\252\190a\242V\211\171\020*]\158\132<9SGmA\162\031-C\216\183{\164v\196\023I\236\127\012o\246l\161;R)\157U\170\251`\134\177\187\204>Z\203Y_\176\156\169\160Q\011\245\022\235zu,\215O\174\213\233\230\231\173\232t\214\244\234\168PX\175"
+    i = math.max(1, i)
+    return (i > #lookup) and nil or string.byte(string.sub(lookup, i, i))
 end
 
 -- Galios field exponent table
-function Qr:getGexpLookup()
-    return {
-        0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1d, 0x3a, 0x74, 0xe8, 0xcd, 0x87, 0x13, 0x26,
-        0x4c, 0x98, 0x2d, 0x5a, 0xb4, 0x75, 0xea, 0xc9, 0x8f, 0x03, 0x06, 0x0c, 0x18, 0x30, 0x60, 0xc0,
-        0x9d, 0x27, 0x4e, 0x9c, 0x25, 0x4a, 0x94, 0x35, 0x6a, 0xd4, 0xb5, 0x77, 0xee, 0xc1, 0x9f, 0x23,
-        0x46, 0x8c, 0x05, 0x0a, 0x14, 0x28, 0x50, 0xa0, 0x5d, 0xba, 0x69, 0xd2, 0xb9, 0x6f, 0xde, 0xa1,
-        0x5f, 0xbe, 0x61, 0xc2, 0x99, 0x2f, 0x5e, 0xbc, 0x65, 0xca, 0x89, 0x0f, 0x1e, 0x3c, 0x78, 0xf0,
-        0xfd, 0xe7, 0xd3, 0xbb, 0x6b, 0xd6, 0xb1, 0x7f, 0xfe, 0xe1, 0xdf, 0xa3, 0x5b, 0xb6, 0x71, 0xe2,
-        0xd9, 0xaf, 0x43, 0x86, 0x11, 0x22, 0x44, 0x88, 0x0d, 0x1a, 0x34, 0x68, 0xd0, 0xbd, 0x67, 0xce,
-        0x81, 0x1f, 0x3e, 0x7c, 0xf8, 0xed, 0xc7, 0x93, 0x3b, 0x76, 0xec, 0xc5, 0x97, 0x33, 0x66, 0xcc,
-        0x85, 0x17, 0x2e, 0x5c, 0xb8, 0x6d, 0xda, 0xa9, 0x4f, 0x9e, 0x21, 0x42, 0x84, 0x15, 0x2a, 0x54,
-        0xa8, 0x4d, 0x9a, 0x29, 0x52, 0xa4, 0x55, 0xaa, 0x49, 0x92, 0x39, 0x72, 0xe4, 0xd5, 0xb7, 0x73,
-        0xe6, 0xd1, 0xbf, 0x63, 0xc6, 0x91, 0x3f, 0x7e, 0xfc, 0xe5, 0xd7, 0xb3, 0x7b, 0xf6, 0xf1, 0xff,
-        0xe3, 0xdb, 0xab, 0x4b, 0x96, 0x31, 0x62, 0xc4, 0x95, 0x37, 0x6e, 0xdc, 0xa5, 0x57, 0xae, 0x41,
-        0x82, 0x19, 0x32, 0x64, 0xc8, 0x8d, 0x07, 0x0e, 0x1c, 0x38, 0x70, 0xe0, 0xdd, 0xa7, 0x53, 0xa6,
-        0x51, 0xa2, 0x59, 0xb2, 0x79, 0xf2, 0xf9, 0xef, 0xc3, 0x9b, 0x2b, 0x56, 0xac, 0x45, 0x8a, 0x09,
-        0x12, 0x24, 0x48, 0x90, 0x3d, 0x7a, 0xf4, 0xf5, 0xf7, 0xf3, 0xfb, 0xeb, 0xcb, 0x8b, 0x0b, 0x16,
-        0x2c, 0x58, 0xb0, 0x7d, 0xfa, 0xe9, 0xcf, 0x83, 0x1b, 0x36, 0x6c, 0xd8, 0xad, 0x47, 0x8e, 0x00
-    }
+function Qr:gexpLookup(i)
+    local lookup = "\001\002\004\008\016\032@\128\029:t\232\205\135\019&L\152-Z\180u\234\201\143\003\006\012\0240`\192\157'N\156%J\1485j\212\181w\238\193\159#F\140\005\010\020(P\160]\186i\210\185o\222\161_\190a\194\153/^\188e\202\137\015\030<x\240\253\231\211\187k\214\177\127\254\225\223\163[\182q\226\217\175C\134\017\034D\136\013\0264h\208\189g\206\129\031>|\248\237\199\147;v\236\197\1513f\204\133\023.\092\184m\218\169O\158\033B\132\021*T\168M\154)R\164U\170I\1469r\228\213\183s\230\209\191c\198\145?~\252\229\215\179{\246\241\255\227\219\171K\1501b\196\1497n\220\165W\174A\130\0252d\200\141\007\014\0288p\224\221\167S\166Q\162Y\178y\242\249\239\195\155+V\172E\138\009\018$H\144=z\244\245\247\243\251\235\203\139\011\022,X\176}\250\233\207\131\0276l\216\173G\142\000"
+    i = math.max(1, i)
+    return (i > #lookup) and nil or string.byte(string.sub(lookup, i, i))
 end
 
 -- 4 per version: number of blocks 1,2; data width; ecc width
-function Qr:getEccblocksLookup()
-    return {
-        1, 0, 19, 7, 1, 0, 16, 10, 1, 0, 13, 13, 1, 0, 9, 17,
-        1, 0, 34, 10, 1, 0, 28, 16, 1, 0, 22, 22, 1, 0, 16, 28,
-        1, 0, 55, 15, 1, 0, 44, 26, 2, 0, 17, 18, 2, 0, 13, 22,
-        1, 0, 80, 20, 2, 0, 32, 18, 2, 0, 24, 26, 4, 0, 9, 16,
-        1, 0, 108, 26, 2, 0, 43, 24, 2, 2, 15, 18, 2, 2, 11, 22,
-        2, 0, 68, 18, 4, 0, 27, 16, 4, 0, 19, 24, 4, 0, 15, 28,
-        2, 0, 78, 20, 4, 0, 31, 18, 2, 4, 14, 18, 4, 1, 13, 26,
-        2, 0, 97, 24, 2, 2, 38, 22, 4, 2, 18, 22, 4, 2, 14, 26,
-        2, 0, 116, 30, 3, 2, 36, 22, 4, 4, 16, 20, 4, 4, 12, 24,
-        2, 2, 68, 18, 4, 1, 43, 26, 6, 2, 19, 24, 6, 2, 15, 28,
-        4, 0, 81, 20, 1, 4, 50, 30, 4, 4, 22, 28, 3, 8, 12, 24,
-        2, 2, 92, 24, 6, 2, 36, 22, 4, 6, 20, 26, 7, 4, 14, 28,
-        4, 0, 107, 26, 8, 1, 37, 22, 8, 4, 20, 24, 12, 4, 11, 22,
-        3, 1, 115, 30, 4, 5, 40, 24, 11, 5, 16, 20, 11, 5, 12, 24,
-        5, 1, 87, 22, 5, 5, 41, 24, 5, 7, 24, 30, 11, 7, 12, 24,
-        5, 1, 98, 24, 7, 3, 45, 28, 15, 2, 19, 24, 3, 13, 15, 30,
-        1, 5, 107, 28, 10, 1, 46, 28, 1, 15, 22, 28, 2, 17, 14, 28,
-        5, 1, 120, 30, 9, 4, 43, 26, 17, 1, 22, 28, 2, 19, 14, 28,
-        3, 4, 113, 28, 3, 11, 44, 26, 17, 4, 21, 26, 9, 16, 13, 26,
-        3, 5, 107, 28, 3, 13, 41, 26, 15, 5, 24, 30, 15, 10, 15, 28,
-        4, 4, 116, 28, 17, 0, 42, 26, 17, 6, 22, 28, 19, 6, 16, 30,
-        2, 7, 111, 28, 17, 0, 46, 28, 7, 16, 24, 30, 34, 0, 13, 24,
-        4, 5, 121, 30, 4, 14, 47, 28, 11, 14, 24, 30, 16, 14, 15, 30,
-        6, 4, 117, 30, 6, 14, 45, 28, 11, 16, 24, 30, 30, 2, 16, 30,
-        8, 4, 106, 26, 8, 13, 47, 28, 7, 22, 24, 30, 22, 13, 15, 30,
-        10, 2, 114, 28, 19, 4, 46, 28, 28, 6, 22, 28, 33, 4, 16, 30,
-        8, 4, 122, 30, 22, 3, 45, 28, 8, 26, 23, 30, 12, 28, 15, 30,
-        3, 10, 117, 30, 3, 23, 45, 28, 4, 31, 24, 30, 11, 31, 15, 30,
-        7, 7, 116, 30, 21, 7, 45, 28, 1, 37, 23, 30, 19, 26, 15, 30,
-        5, 10, 115, 30, 19, 10, 47, 28, 15, 25, 24, 30, 23, 25, 15, 30,
-        13, 3, 115, 30, 2, 29, 46, 28, 42, 1, 24, 30, 23, 28, 15, 30,
-        17, 0, 115, 30, 10, 23, 46, 28, 10, 35, 24, 30, 19, 35, 15, 30,
-        17, 1, 115, 30, 14, 21, 46, 28, 29, 19, 24, 30, 11, 46, 15, 30,
-        13, 6, 115, 30, 14, 23, 46, 28, 44, 7, 24, 30, 59, 1, 16, 30,
-        12, 7, 121, 30, 12, 26, 47, 28, 39, 14, 24, 30, 22, 41, 15, 30,
-        6, 14, 121, 30, 6, 34, 47, 28, 46, 10, 24, 30, 2, 64, 15, 30,
-        17, 4, 122, 30, 29, 14, 46, 28, 49, 10, 24, 30, 24, 46, 15, 30,
-        4, 18, 122, 30, 13, 32, 46, 28, 48, 14, 24, 30, 42, 32, 15, 30,
-        20, 4, 117, 30, 40, 7, 47, 28, 43, 22, 24, 30, 10, 67, 15, 30,
-        19, 6, 118, 30, 18, 31, 47, 28, 34, 34, 24, 30, 20, 61, 15, 30
-    }
+function Qr:eccblocksLookup(i)
+    -- only the first 14 lines (56 lookups)
+    local lookup = "\001\000\019\007\001\000\016\010\001\000\013\013\001\000\009\017\001\000\034\010\001\000\028\016\001\000\022\022\001\000\016\028\001\0007\015\001\000,\026\002\000\017\018\002\000\013\022\001\000P\020\002\000\032\018\002\000\024\026\004\000\009\016\001\000l\026\002\000+\024\002\002\015\018\002\002\011\022\002\000D\018\004\000\027\016\004\000\019\024\004\000\015\028\002\000N\020\004\000\031\018\002\004\014\018\004\001\013\026\002\000a\024\002\002&\022\004\002\018\022\004\002\014\026\002\000t\030\003\002$\022\004\004\016\020\004\004\012\024\002\002D\018\004\001+\026\006\002\019\024\006\002\015\028\004\000Q\020\001\0042\030\004\004\022\028\003\008\012\024\002\002\092\024\006\002$\022\004\006\020\026\007\004\014\028\004\000k\026\008\001%\022\008\004\020\024\012\004\011\022"
+    i = math.max(1, i) --for some reason it starts at negative numbers
+    return (i > #lookup) and nil or string.byte(string.sub(lookup, i, i))
 end
 
 -- alignment pattern (used once)
 function Qr:adeltaLookup(i)
-    local tbl = {
-        0, 11, 15, 19, 23, 27, 31, --force 1 pat
-        16, 18, 20, 22, 24, 26, 28, 20, 22, 24, 24, 26, 28, 28, 22, 24, 24,
-        26, 26, 28, 28, 24, 24, 26, 26, 26, 28, 28, 24, 26, 26, 26, 28, 28
-    }
-    return tbl[i + 1]
+    local lookup = "\000\011\015\019\023\027\031\016\018\020\022\024\026\028\020\022\024\024\026\028\028\022\024\024\026\026\028\028\024\024\026\026\026\028\028\024\026\026\026\028\028"
+    i = math.max(1, i + 1)
+    return (i > #lookup) and nil or string.byte(string.sub(lookup, i, i))
 end
 
 -- version block (used once)
@@ -285,20 +214,19 @@ function Qr:genframe(instring)
     if self.progress == 1 then
         if self.resume == nil then self.resume = {vsn = 0} end
         -- find the smallest version that fits the string
-        local lookup = self:getEccblocksLookup()
         for vsn = self.resume.vsn, 39 do
             if getUsage() > 60 then
                 self.resume.vsn = vsn
                 return
             end
             local k = (self.ecclevel - 1) * 4 + (vsn - 1) * 16
-            self.neccblk1 = lookup[k + 1]
+            self.neccblk1 = self:eccblocksLookup(k + 1)
             k = k + 1
-            self.neccblk2 = lookup[k + 1]
+            self.neccblk2 = self:eccblocksLookup(k + 1)
             k = k + 1
-            self.datablkw = lookup[k + 1]
+            self.datablkw = self:eccblocksLookup(k + 1)
             k = k + 1
-            self.eccblkwid = lookup[k + 1]
+            self.eccblkwid = self:eccblocksLookup(k + 1)
             k = self.datablkw * (self.neccblk1 + self.neccblk2) + self.neccblk2 - 3 + (self.version <= 9 and 1 or 0)
             if #instring <= k then
                 self.version = vsn
@@ -514,7 +442,7 @@ function Qr:genframe(instring)
         -- calculate generator polynomial
         if self.resume == nil then --first time through
             self.genpoly[0] = 1;
-            self.resume = {i=0, j=0, gexp = self:getGexpLookup(), glog=self:getGlogLookup()} --j for 2nd loop below
+            self.resume = {i=0, j=0} --j for 2nd loop below
         end
         local ctx = self.resume
         for i = ctx.i, self.eccblkwid - 1 do
@@ -522,15 +450,15 @@ function Qr:genframe(instring)
             if getUsage() > 40 then return end
             self.genpoly[i + 1] = 1;
             for j = i, 1, -1 do
-                self.genpoly[j] = (self.genpoly[j] >= 1) and bit32.bxor(self.genpoly[j - 1], ctx.gexp[1 + self:modnn(ctx.glog[1 + self.genpoly[j]] + i)]) or self.genpoly[j - 1] --^
+                self.genpoly[j] = (self.genpoly[j] >= 1) and bit32.bxor(self.genpoly[j - 1], self:gexpLookup(1 + self:modnn(self:glogLookup(1 + self.genpoly[j]) + i))) or self.genpoly[j - 1] --^
             end
-            self.genpoly[0] = ctx.gexp[1 + self:modnn(ctx.glog[1 + self.genpoly[0]] + i)]
+            self.genpoly[0] = self:gexpLookup(1 + self:modnn(self:glogLookup(1 + self.genpoly[0]) + i))
         end
         ctx.i = ctx.i + 1 --increment once more
         for j = ctx.j, self.eccblkwid do --inclusive
             ctx.j = j
             if getUsage() > 40 then return end
-            self.genpoly[j] = ctx.glog[1 + self.genpoly[j]]; -- use logs for genpoly[] to save calc step
+            self.genpoly[j] = self:glogLookup(1 + self.genpoly[j]); -- use logs for genpoly[] to save calc step
         end
         -- don't clear context, next step wants the lookup tables
         if table ~= nil then print("QR: genpoly", table.unpack(self.genpoly)) end --desktop only
@@ -542,7 +470,7 @@ function Qr:genframe(instring)
     if self.progress == 7 then
         -- append ecc to data buffer
         if self.resume.blk == nil then
-            self.resume = {blk=0, j=0, k=self.maxlength, y=0, gexp = self.resume.gexp, glog=self.resume.glog}
+            self.resume = {blk=0, j=0, k=self.maxlength, y=0}
         end
         local ctx = self.resume
         for blk = ctx.blk, 1 do
@@ -558,17 +486,17 @@ function Qr:genframe(instring)
                 for id = ctx.id, self.datablkw + blk - 1 do
                     ctx.id = id
                     if getUsage() > 60 then return end
-                    local fb = ctx.glog[1 + bit32.bxor(self.strinbuf[ctx.y + id], self.strinbuf[ctx.k])] --^
+                    local fb = self:glogLookup(1 + bit32.bxor(self.strinbuf[ctx.y + id], self.strinbuf[ctx.k])) --^
                     if fb ~= 255 then     --fb term is non-zero
                         for jd = 1, self.eccblkwid - 1 do
-                            self.strinbuf[ctx.k + jd - 1] = bit32.bxor(self.strinbuf[ctx.k + jd], ctx.gexp[1 + self:modnn(fb + self.genpoly[self.eccblkwid - jd])]) --^
+                            self.strinbuf[ctx.k + jd - 1] = bit32.bxor(self.strinbuf[ctx.k + jd], self:gexpLookup(1 + self:modnn(fb + self.genpoly[self.eccblkwid - jd]))) --^
                         end
                     else
                         for jd = ctx.k, ctx.k + self.eccblkwid - 1 do
                             self.strinbuf[jd] = self.strinbuf[jd + 1]
                         end
                     end
-                    self.strinbuf[ctx.k + self.eccblkwid - 1] = fb == 255 and 0 or ctx.gexp[1 + self:modnn(fb + self.genpoly[0])]
+                    self.strinbuf[ctx.k + self.eccblkwid - 1] = fb == 255 and 0 or self:gexpLookup(1 + self:modnn(fb + self.genpoly[0]))
                 end
                 ctx.id = nil
                 ctx.y = ctx.y + self.datablkw + blk
